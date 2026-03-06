3c6fcfbb8eb8baf68873dfac0c53ebb32b0ebb95
al-ameen24434 <alameenabdulrahmon@gmail.com>
2026-03-06 22:01:48 +0100

    fix deployment error

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,13 @@ import app from "./app";
 import { logger } from "./utils/logger";
 import { log } from "./utils/debug";
 
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(PORT, () => {
-  logger.info(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-  log.app("server start", { port: PORT, mode: process.env.NODE_ENV });
-});
-
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err: Error) => {
   logger.error(`Unhandled Rejection: ${err.message}`);
   logger.error(err.stack);
   log.error("unhandledRejection", err.message);
-  server.close(() => process.exit(1));
+  // In serverless, let the function fail instead of exiting
+  throw err;
 });
 
 // Handle uncaught exceptions
@@ -22,25 +16,8 @@ process.on("uncaughtException", (err: Error) => {
   logger.error(`Uncaught Exception: ${err.message}`);
   logger.error(err.stack);
   log.error("uncaughtException", err.message);
-  process.exit(1);
+  // In serverless, let the function fail instead of exiting
+  throw err;
 });
 
-// Graceful shutdown
-const gracefulShutdown = () => {
-  logger.info("Received shutdown signal, closing server...");
-  server.close(() => {
-    logger.info("Server closed");
-    process.exit(0);
-  });
-
-  // Force close after 10 seconds
-  setTimeout(() => {
-    logger.error(
-      "Could not close connections in time, forcefully shutting down",
-    );
-    process.exit(1);
-  }, 10000);
-};
-
-process.on("SIGTERM", gracefulShutdown);
-process.on("SIGINT", gracefulShutdown);
+export default app;
